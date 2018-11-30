460a18bc9cddf206f1231aa3b822ca6e59b12163
tom@tom-HP-EliteBook-8570p <tom.goyens@gmail.com>
2018-11-30 15:45:45 +0100

    kmoet eigenlijk og mixins uitproberen

---
+++ assets/js/back-end.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        document.getElementById("header").style.opacity = "0.8";
+    } else {
+        document.getElementById("header").style.opacity = "1";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
